abe8a5c3072da2e9ca91fedbc2fffb44989670a2
Sergei <iamsergf@gmail.com>
2022-01-19 14:00:40 +0300

    first cumm

---
+++ efei.py
@@ -0,0 +1,10 @@
+def percents(x,y):
+    one_percent = x + 100
+    result = y * one_percent
+    return result
+
+
+def print_percents(x,y):
+   print(str(y) + ' is ' + str(percents(x,y)) + " % of " + str(x))
+
+print_percents (2000,50)
